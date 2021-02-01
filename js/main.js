999ef73c05f5c135e23fc5e26a521bf45112ade9
Mihanen <46423421+Mihanen@users.noreply.github.com>
2021-02-01 23:11:46 +0500

    функции 1.1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
-const random = function (min, max){
+/* eslint-disable no-console */
+const getRandomRange = function (min, max){
   min = Math.ceil(min);
   max = Math.floor(max);
-  if (min === max) {alert('Выполнение невозможно')}
-  else if (min < 0) {alert('Выполнение невозможно')}
-  else if (min > max) {alert('Выполнение невозможно')}
+  if (min === max) { console.log('Выполнение невозможно')}
+  else if (min < 0) { console.log('Выполнение невозможно')}
+  else if (min > max) { console.log('Выполнение невозможно')}
   else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
-alert(random(1, 7));
+console.log(getRandomRange(1, 52));
 
-const randomNotRound = function (min, max ) {
-  if (min > max) { alert ('Выполнение невозможно')}
-  else if (min < 0) { alert ('Выполнение невозможно')}
-  else if (min === max) {alert ('Выполнение невозможно')}
-  else
-  {
-    return Math.round (Math.random() * (max - min) + min);
-  }
+const getLineLength = function (){
+  const string = 'Строка';
+  const checkedString = string.length;
+  const maxLineRange = 140;
+  if (checkedString < maxLineRange) {return true}
+  else {return false}
 }
-alert (randomNotRound(15, 5));
-//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Statements/if...else
+console.log(getLineLength())
+
