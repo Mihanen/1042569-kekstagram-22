9d67083bb7f6af12da45c63f7be2bd036c7ac55f
Mihanen <46423421+Mihanen@users.noreply.github.com>
2021-02-03 18:00:51 +0500

    1.3

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+// eslint-disable-next-line no-unused-vars
 const getRandomRange = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -9,6 +10,7 @@ const getRandomRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+// eslint-disable-next-line no-unused-vars
 const getLineLength = function (string, maxLineRange){
   if (string.length < maxLineRange) {return true}
   else {return false}
